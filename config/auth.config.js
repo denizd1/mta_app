aad8d7eae89eadfc6a83ca2bca95868d375852ac
Deniz <denzdonmez@gmail.com>
2022-03-16 09:20:23 +0300

    update

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    secret:'your_secret_key',
+    secret: "6VQG6+MBzcBw9WgtMaGX55FRNlZfxB2dnNs+nY0PPVV6GvQEdLhrFKTVC0EUQ1U5vol/SROeEEz4RGU7FLgtpyBBcbGGr78FOj4cwDsf7AzsZR2G86it+XaA5KOsh/W9i+32dVwAvplt98quVRkSZ9aktCq+FSTkmEV9QJx6NXaq5FfPFi5SaAaru7ExA1smZhzUx4Tt/2Yi2YsQy3IP2z7KupDHMxTszYKBdPLkqYPOI3aAWXdMUwVPcCdO8bkHpjn7evugbR6bUqdWGbIwIlCBAwQFxQI0YX0WCgN/C+qUdV+Q4+/f28OFaZ8ZMCc3OhNp0Nptgfi8881AvJ0AHw==",
     jwtExpiration: 60,           // 1 hour
     jwtRefreshExpiration: 120,   // 24 hours
   
