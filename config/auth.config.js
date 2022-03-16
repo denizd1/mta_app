07e3b39c7f96ab67261ebcd530653998e5454102
Deniz <denzdonmez@gmail.com>
2022-03-16 09:34:50 +0300

    auth config

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,5 +1,9 @@
+const crypto = require('crypto');
+const secret = 'c0fa1bc00531bd78ef38c628449c5102aeabd49b5dc3a2a516ea6ea959d6658e';
 module.exports = {
-    secret: "6VQG6+MBzcBw9WgtMaGX55FRNlZfxB2dnNs+nY0PPVV6GvQEdLhrFKTVC0EUQ1U5vol/SROeEEz4RGU7FLgtpyBBcbGGr78FOj4cwDsf7AzsZR2G86it+XaA5KOsh/W9i+32dVwAvplt98quVRkSZ9aktCq+FSTkmEV9QJx6NXaq5FfPFi5SaAaru7ExA1smZhzUx4Tt/2Yi2YsQy3IP2z7KupDHMxTszYKBdPLkqYPOI3aAWXdMUwVPcCdO8bkHpjn7evugbR6bUqdWGbIwIlCBAwQFxQI0YX0WCgN/C+qUdV+Q4+/f28OFaZ8ZMCc3OhNp0Nptgfi8881AvJ0AHw==",
+    secret: crypto.createHmac('sha512', secret)
+    .update('superdupersecretkey')
+    .digest('hex'),
     jwtExpiration: 60,           // 1 hour
     jwtRefreshExpiration: 120,   // 24 hours
   
