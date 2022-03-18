d4a05bbde4d691ff3f7fff84109d509a3eed43eb
Deniz <denzdonmez@gmail.com>
2022-03-18 10:34:13 +0300

    build

--- server.js
+++ server.js
@@ -20,13 +20,14 @@ var corsOptions = {
 //Limiter
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 10000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
 
-app.use(helmet());
+
+
 app.use(limiter)
 app.use(express.static(path));
 app.use(cors(corsOptions));
@@ -34,6 +35,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(cookieParser())
+app.use(helmet());
+
+
+
 
 
 
