6e98ed53f90ace5a68827daa15b48473eae5127b
Deniz <denzdonmez@gmail.com>
2022-03-28 09:15:58 +0300

    cors update

--- server.js
+++ server.js
@@ -3,49 +3,44 @@ const cors = require("cors");
 // const bodyParser = require('body-parser');
 // const cookieParser = require('cookie-parser')
 
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit')
-const db = require(__dirname +"/models");
-const fs = require('fs');
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const db = require(__dirname + "/models");
+const fs = require("fs");
 
-const path = __dirname + '/views/';
+const path = __dirname + "/views/";
 const app = express();
 const Role = db.role;
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
+// var whitelist = ["http://0.0.0.0:8081"];
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+//     callback(null, originIsWhitelisted);
+//   },
+//   credentials: true,
+// };
+app.use(cors());
 
 //Limiter
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 10000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
-
-
-
-app.use(limiter)
+app.use(limiter);
 app.use(express.static(path));
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(cookieParser())
 app.use(helmet());
 
-
-
-
-
-
-require(__dirname +"/routes/tutorial.routes")(app);
-require(__dirname +'/routes/auth.routes')(app);
-require(__dirname +'/routes/user.routes')(app);
-
+require(__dirname + "/routes/tutorial.routes")(app);
+require(__dirname + "/routes/auth.routes")(app);
+require(__dirname + "/routes/user.routes")(app);
 
 db.sequelize.sync();
 // db.sequelize.sync({force: true}).then(() => {
@@ -53,29 +48,27 @@ db.sequelize.sync();
 //   initial();
 // });
 
-app.get('/', function (req,res) {
+app.get("/", function (req, res) {
   res.sendFile(path + "index.html");
 });
 
-app.get('/api/getGeoJson:val', function(req, res) {
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  val = req.params.val
-  if(val == 0){
-    fs.createReadStream(__dirname + '/tr-cities-utf8.geojson').pipe(res);
+app.get("/api/getGeoJson:val", function (req, res) {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  val = req.params.val;
+  if (val == 0) {
+    fs.createReadStream(__dirname + "/tr-cities-utf8.geojson").pipe(res);
   }
-  if(val == 25){
-    fs.createReadStream(__dirname + '/pafta25000.geojson').pipe(res);
+  if (val == 25) {
+    fs.createReadStream(__dirname + "/pafta25000.geojson").pipe(res);
   }
-  
-  if(val == 100){
-    fs.createReadStream(__dirname + '/pafta100000.geojson').pipe(res);
-  }
-  if(val == 500){
-    fs.createReadStream(__dirname + '/pafta500000.geojson').pipe(res);
-  }
-  
-});
 
+  if (val == 100) {
+    fs.createReadStream(__dirname + "/pafta100000.geojson").pipe(res);
+  }
+  if (val == 500) {
+    fs.createReadStream(__dirname + "/pafta500000.geojson").pipe(res);
+  }
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
@@ -86,16 +79,16 @@ app.listen(PORT, () => {
 function initial() {
   Role.create({
     id: 1,
-    name: "user"
+    name: "user",
   });
- 
+
   Role.create({
     id: 2,
-    name: "moderator"
+    name: "moderator",
   });
- 
+
   Role.create({
     id: 3,
-    name: "admin"
+    name: "admin",
   });
 }
